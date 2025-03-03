ae4ecf6516c503928b5e8e7d12280e9a413321c6
ahmmohammed <Ahmedx39311@gmail.com>
2025-03-03 05:23:28 +0300

    storage folder error handle

---
+++ pkg/util/storage.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"os"
+	consts "forum/pkg/consts"
+)
+
+// InitializeStorage ensures the storage directory exists
+func InitializeStorage() error {
+	// Create base storage directory if it doesn't exist
+	if err := os.MkdirAll(consts.STORAGE, 0755); err != nil {
+		return err
+	}
+	return nil
+}
