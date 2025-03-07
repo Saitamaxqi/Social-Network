d90532b0817dbee923b8ce9e9fc8af19ccfc548c
ryousif <roro2004yousif@gmail.com>
2025-03-08 00:32:53 +0300

    ba8i avatar + edit profile + if the acc private

---
+++ web/src/app/profile/[id]/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { ProfilePage } from '@/components/Profile/Profile';
+import MainLayout from '@/components/Layout/MainLayout';
+
+export default function Profile(){
+    return (
+            <MainLayout>
+        
+        <ProfilePage></ProfilePage>
+        </MainLayout>
+    )
+}
